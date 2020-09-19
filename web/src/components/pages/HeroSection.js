56e65e2de716312ad1a5851088723f94f82b20ca
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-19 14:08:21 -0300

    Correções estrutura html; Adicionado spread para os novos objets.

--- web/src/components/pages/HeroSection.js
+++ web/src/components/pages/HeroSection.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 
-import imagem from '../../assets/images/svg-1.svg';
 import './HeroSection.css';
 
 function HeroSection({
@@ -14,34 +13,38 @@ function HeroSection({
         <>
             <div className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}>
                 <div className='container'>
-
-                    <div className='col'>
-                        <div className='home__hero-text-wrapper'>
-                            <div className='top-line'>
-                                {topLine}
+                    <div className='row home__hero-row'
+                    style={{display: 'flex', flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'}}
+                    >
+                    
+                        <div className="col">
+                            <div className='home__hero-text-wrapper'>
+                                <div className='top-line'>
+                                    {topLine}
+                                </div>
+                                <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
+                                <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>
+                                    {description}
+                                </p>
+                                    <Link to='/sign-up'>
+                                        <Button buttonSize='btn--wide' buttonColor='blue'>
+                                            {buttonLabel}
+                                        </Button>
+                                    </Link>
                             </div>
-                            <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
-                            <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>
-                                {description}
-                                <Link to='/sign-up'>
-                                    <Button buttonSize='btn--wide' buttonColor='blue'>
-                                        {buttonLabel}
-                                    </Button>
-                                </Link>
-                            </p>
                         </div>
-                    </div>
 
-                    <div className='col'>
-                        <div className='home__hero-img-wrapper'>
-                            <img src={imagem} alt={alt} className='home__hero-img'/>
+                        <div className="col">
+                            <div className='home__hero-img-wrapper'>
+                                <img src={img} alt={alt} className='home__hero-img'/>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
 export default HeroSection;

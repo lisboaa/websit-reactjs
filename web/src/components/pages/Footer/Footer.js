2523385547fcc075c942a7c034bf505c99166926
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-19 18:39:39 -0300

    Correções nomeclatura

--- web/src/components/pages/Footer/Footer.js
+++ web/src/components/pages/Footer/Footer.js
@@ -16,10 +16,10 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join our exclusive membership to receive the latest news and trends
+        Junte-se à nossa assinatura exclusiva para receber as últimas notícias e tendências
         </p>
         <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
+        Você pode cancelar sua inscrição a qualquer momento.
         </p>
         <div className='input-areas'>
           <form>
@@ -27,40 +27,40 @@ function Footer() {
               className='footer-input'
               name='email'
               type='email'
-              placeholder='Your Email'
+              placeholder='E-mail'
             />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
+            <Button buttonStyle='btn--outline'>Inscreva-se</Button>
           </form>
         </div>
       </section>
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
+            <h2>Sobre nós</h2>
+            <Link to='/sign-up'>Como funciona</Link>
+            <Link to='/'>Testemunhos</Link>
+            <Link to='/'>Carreiras</Link>
+            <Link to='/'>Investidoras(es)</Link>
+            <Link to='/'>Termos de serviço</Link>
           </div>
           <div className='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
+            <h2>Contate-Nos</h2>
+            <Link to='/'>Contato</Link>
             <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
+            <Link to='/'>Destinatário</Link>
+            <Link to='/'>Patrocínios</Link>
           </div>
         </div>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
             <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
+            <Link to='/'>Enviar Vídeo</Link>
+            <Link to='/'>Embaixadores</Link>
+            <Link to='/'>Agência</Link>
+            <Link to='/'>Influenciadores</Link>
           </div>
           <div className='footer-link-items'>
-            <h2>Social Media</h2>
+            <h2>Mídia social</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
             <Link to='/'>Youtube</Link>
@@ -73,10 +73,10 @@ function Footer() {
           <div className='footer-logo'>
             <Link to='/' className='social-logo'>
               <MdFingerprint className='navbar-icon' />
-              LAVISH
+              Generoso
             </Link>
           </div>
-          <small className='website-rights'>LAVISH © 2020</small>
+          <small className='website-rights'>Generoso © 2020</small>
           <div className='social-icons'>
             <Link
               className='social-icon-link'

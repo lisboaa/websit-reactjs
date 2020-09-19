5820a3992a3f94df66c38ab3b9d6f5b2625f53b4
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-19 14:12:37 -0300

    Correção porcentagem flex-basic; Adicionado media; Adicionado strutura css global; Adicionado layout da home e exact path.

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import './assets/css/global.css'
+
 import Navbar from '../src/components/pages/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './components/pages/HomePage/Home';
+
 function App() {
   return (
     <Router>
       <Navbar/>
       <Switch>
-        <Route path='/'/>
+        <Route path='/' exact component={Home}/>
       </Switch>
     </Router>
   );

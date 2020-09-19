34e0a4b6e481350ff452c296ab4f910ea3428d45
Douglas <douglas.lisboa@edu.unipar.br>
2020-09-19 18:44:42 -0300

    Correção size font p; Adicionado novos components e rotas.

--- web/src/App.js
+++ web/src/App.js
@@ -6,6 +6,10 @@ import Navbar from '../src/components/pages/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/HomePage/Home';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+import Footer from './components/pages/Footer/Footer';
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
       <Navbar/>
       <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/services' exact component={Services}/>
+        <Route path='/products' exact component={Products}/>
+        <Route path='/sign-up' exact component={SignUp}/>
       </Switch>
+      <Footer />
     </Router>
   );
 }
